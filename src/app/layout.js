baa42284b18508a1f6403b2f58a287c30637dfd9
ewwmonya <savvymallen85@gmail.com>
2025-07-27 17:40:32 -0400

    fix no link in footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 					</aside>
 
 					<nav className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
-						<a>
+						{/* <a>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
 								width='24'
@@ -69,7 +69,7 @@ export default function RootLayout({ children }) {
 							>
 								<path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z'></path>
 							</svg>
-						</a>
+						</a> */}
 					</nav>
 				</footer>
 			</body>
